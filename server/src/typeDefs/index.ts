9ccaa9234a8b1da3d5a937516359b0082d7692f4
Kyle Grymonpre <kgrymonpr@gmail.com>
2021-06-21 21:21:24 -0500

    Move types to .graphql files.

--- server/src/typeDefs/index.ts
+++ server/src/typeDefs/index.ts
@@ -1,22 +1,17 @@
+import path from 'path';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
-import userTypeDefs from './user';
+const typesArray = loadFilesSync(
+    path.join(__dirname, '.'),
+    { extensions: ['graphql'] },
+);
 
-const baseTypeDefs = `
-    type Query {
-        _empty: String
-    }
-
-    type Mutation {
-        _empty: String
-    }
-`;
+const mergedTypeDefs = mergeTypeDefs(typesArray);
 
 export default [
     makeExecutableSchema({
-        typeDefs: [
-            baseTypeDefs,
-            userTypeDefs,
-        ],
-   }),
+        typeDefs: mergedTypeDefs,
+    }),
 ];
